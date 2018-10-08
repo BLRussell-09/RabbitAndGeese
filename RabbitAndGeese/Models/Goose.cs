f8603432c2a771e0b453aaf23400dad6c2c3d62d
Barry Russell <blrussell09@gmail.com>
2018-10-08 18:16:54 -0500

    stashing

--- RabbitAndGeese/Models/Goose.cs
+++ RabbitAndGeese/Models/Goose.cs
@@ -1,9 +1,14 @@
-﻿namespace RabbitAndGeese.Models
+﻿using System.Collections.Generic;
+
+namespace RabbitAndGeese.Models
 {
     public class Goose
     {
         public string Name { get; set; }
         public Sex Sex { get; set; }
+        public Size Size { get; set; }
         public bool Social { get; set; }
-    }
+        public Saddle Saddle { get; set; }
+        public string EmotionalState { get; set; }
+  }
 }
--- RabbitAndGeese/Models/Rabbit.cs
+++ RabbitAndGeese/Models/Rabbit.cs
@@ -1,10 +1,36 @@
-﻿namespace RabbitAndGeese.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace RabbitAndGeese.Models
 {
     public class Rabbit
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
         public string Color { get; set; }
         public int MaxFeetPerSecond { get; set; }
-        public EarSize EarSize { get; set; }
+        public Size Size { get; set; }
         public Sex Sex { get; set; }
+        public List<Goose> OwnedGeese { get; set; } = new List<Goose>();
+        public List<Saddle> OwnedSaddles { get; set; } = new List<Saddle>();
+
+        public void SaddleThatGoose(Goose goose, Saddle saddle)
+        {
+            if (goose.Saddle != null)
+            {
+                goose.EmotionalState = "Exhausted";
+                return;
+            };
+
+            if (goose.Size == saddle.Size)              a
+            {
+                goose.Saddle = saddle;
+                saddle.InUse = true;
+            }
+            else if (goose.Size > saddle.Size)
+            {
+                goose.EmotionalState = "Distraught by your fat shaming";
+            }
+        }
     }
 }
